4410eb9e699a789ae2173f9db4ed67b027a59cae
hemanth <phemanth4647@gmail.com>
2025-06-07 17:16:00 +0530

    hosting changes 1

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized:false,
+  }
   // user: 'hemanth',
   // host: 'localhost',
   // database: 'lib1',//!!!!!!!!!!!!!!!!!!!!!!!database not table name !!!!!!!!!!!!!!!!!!!!!!!!!!
--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,15 @@ const booksController = require("../controller/booksController");
 router.get("/", booksController.showHello); //example code
 router.get("/show", booksController.showBooksSample); //example code
 router.get("/data", booksController.showData); //example code
-
+router.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ success: true, db_time: result.rows[0].now });
+  } catch (err) {
+    console.error("DB connection error:", err);
+    res.status(500).json({ success: false, message: "DB Error", error: err.message });
+  }
+});
 router.get("/all", booksController.getAllBooks); //r
 router.get("/:id", booksController.getBookById); //r by id
 router.get("/issue/logs/book/:id", booksController.bookIssueLog); //r by id
